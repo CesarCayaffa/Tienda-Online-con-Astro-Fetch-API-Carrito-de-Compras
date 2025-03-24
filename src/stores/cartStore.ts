d9c63911a116f5932b53833f0d8b5b9c1e60848e
CesarCayaffa <cesarcayaffa0@gmail.com>
2025-03-24 15:37:35 -0400

    Agregar store de carrito utilizando Zustand para gestionar la adición, eliminación y limpieza de productos en el carrito

---
+++ src/stores/cartStore.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+
+interface CartItem {
+    id: number;
+    tittle: string;
+    price: number;
+    quantity: number;
+}
+
+interface CartStore {
+    cart: CartItem[];
+    addToCart: (item: CartItem) => void;
+    removeFromCart: (id: number) => void;
+    clearCart: () => void;
+}
+
+export const useCartStore = create<CartStore>((set) => ({
+    cart: [],
+    addToCart: (item) =>
+        set((state) => {
+          const existingItem = state.cart.find((cartItem) => cartItem.id === item.id);
+          if (existingItem) {
+            // Incrementar la cantidad si el producto ya está en el carrito
+            return {
+              cart: state.cart.map((cartItem) =>
+                cartItem.id === item.id
+                  ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                  : cartItem
+              ),
+            };
+          }
+          // Agregar un nuevo producto al carrito
+          return { cart: [...state.cart, { ...item, quantity: 1 }] };
+        }),
+      removeFromCart: (id) =>
+        set((state) => ({
+          cart: state.cart.filter((cartItem) => cartItem.id !== id),
+        })),
+      clearCart: () => set({ cart: [] }),
+    }));
