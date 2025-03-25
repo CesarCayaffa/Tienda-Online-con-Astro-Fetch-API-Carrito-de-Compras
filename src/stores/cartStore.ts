4a49c27dccfabefe26552eb9a23847b32df7dc0d
CesarCayaffa <cesarcayaffa0@gmail.com>
2025-03-25 19:09:01 -0400

    Agregar función para eliminar un artículo del carrito y ajustar la cantidad en el componente cartStore

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -2,47 +2,58 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface CartItem {
-    id: number;
-    title: string;
-    price: number;
-    quantity: number;
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
 }
 
 interface CartStore {
-    cart: CartItem[];
-    addToCart: (item: CartItem) => void;
-    removeFromCart: (id: number) => void;
-    clearCart: () => void;
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+  removeOneFromCart: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  clearCart: () => void;
 }
 
 export const useCartStore = create(
-    persist<CartStore>(
-        (set) => ({
-            cart: [],
-            addToCart: (item) =>
-                set((state) => {
-                    const existingItem = state.cart.find(
-                        (cartItem) => cartItem.id === item.id
-                    );
-                    if (existingItem) {
-                        return {
-                            cart: state.cart.map((cartItem) =>
-                                cartItem.id === item.id
-                                    ? { ...cartItem, quantity: cartItem.quantity + 1 }
-                                    : cartItem
-                            ),
-                        };
-                    }
-                    return { cart: [...state.cart, { ...item, quantity: 1 }] };
-                }),
-            removeFromCart: (id) =>
-                set((state) => ({
-                    cart: state.cart.filter((cartItem) => cartItem.id !== id),
-                })),
-            clearCart: () => set({ cart: [] }),
+  persist<CartStore>(
+    (set) => ({
+      cart: [],
+      addToCart: (item) =>
+        set((state) => {
+          const existingItem = state.cart.find(
+            (cartItem) => cartItem.id === item.id
+          );
+          if (existingItem) {
+            return {
+              cart: state.cart.map((cartItem) =>
+                cartItem.id === item.id
+                  ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                  : cartItem
+              ),
+            };
+          }
+          return { cart: [...state.cart, { ...item, quantity: 1 }] };
         }),
-        {
-            name: "cart-storage", // Nombre de la clave en localStorage
-        }
-    )
+      removeOneFromCart: (id) =>
+        set((state) => ({
+          cart: state.cart
+            .map((cartItem) =>
+              cartItem.id === id
+                ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem
+            )
+            .filter((cartItem) => cartItem.quantity > 0), // Elimina productos con cantidad 0
+        })),
+      removeFromCart: (id) =>
+        set((state) => ({
+          cart: state.cart.filter((cartItem) => cartItem.id !== id),
+        })),
+      clearCart: () => set({ cart: [] }),
+    }),
+    {
+      name: "cart-storage", // Nombre de la clave en localStorage
+    }
+  )
 );
