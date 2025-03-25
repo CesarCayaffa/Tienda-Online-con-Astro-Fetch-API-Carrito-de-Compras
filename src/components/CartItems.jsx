c6c97d22bf0764be068bf65d220a9dbd14176f45
CesarCayaffa <cesarcayaffa0@gmail.com>
2025-03-25 19:09:07 -0400

    Agregar controles para modificar la cantidad de artículos en el carrito y eliminar artículos

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -3,10 +3,14 @@ import { useCartStore } from "../stores/cartStore";
 
 const CartItems = () => {
   const cart = useCartStore((state) => state.cart);
+  const addToCart = useCartStore((state) => state.addToCart);
+  const removeOneFromCart = useCartStore((state) => state.removeOneFromCart);
+  const removeFromCart = useCartStore((state) => state.removeFromCart);
 
   if (cart.length === 0) {
     return <p className="text-gray-600">El carrito está vacío.</p>;
   }
+
   const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
   return (
@@ -18,7 +22,30 @@ const CartItems = () => {
         >
           <div>
             <h2 className="font-bold">{item.title}</h2>
-            <p className="text-gray-600">Cantidad: {item.quantity}</p>
+            <div className="flex justify-between items-center gap-2 mt-2 border-2 border-gray-300 rounded-3xl max-w-[160px]">
+              <button
+                onClick={() => removeOneFromCart(item.id)}
+                className="px-2 font-black text-red-600"
+              >
+                -
+              </button>
+              <p className="text-gray-600">Cantidad: {item.quantity}</p>
+
+              <button
+                onClick={() => addToCart({ ...item, quantity: 1 })}
+                className="px-2 font-black text-green-600"
+              >
+                +
+              </button>
+            </div>
+            <div className="flex items-center gap-2 mt-2">
+              <button
+                onClick={() => removeFromCart(item.id)}
+                className="bg-gray-500 text-white px-2 py-1 rounded hover:bg-gray-600 transition"
+              >
+                Eliminar
+              </button>
+            </div>
           </div>
           <p className="font-bold">${item.price * item.quantity}</p>
         </div>
