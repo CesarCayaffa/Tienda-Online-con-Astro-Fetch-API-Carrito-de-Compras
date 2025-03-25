548f9f2295c4403fde563277747f41e1271393be
CesarCayaffa <cesarcayaffa0@gmail.com>
2025-03-25 19:31:03 -0400

    Mejorar la interfaz del componente CartItems: reorganizar controles de cantidad y precio, y actualizar estilos de botones

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -22,32 +22,37 @@ const CartItems = () => {
         >
           <div>
             <h2 className="font-bold">{item.title}</h2>
-            <div className="flex justify-between items-center gap-2 mt-2 border-2 border-gray-300 rounded-3xl max-w-[160px]">
-              <button
-                onClick={() => removeOneFromCart(item.id)}
-                className="px-2 font-black text-red-600"
-              >
-                -
-              </button>
-              <p className="text-gray-600">Cantidad: {item.quantity}</p>
-
-              <button
-                onClick={() => addToCart({ ...item, quantity: 1 })}
-                className="px-2 font-black text-green-600"
-              >
-                +
-              </button>
-            </div>
-            <div className="flex items-center gap-2 mt-2">
+            <div className="flex items-center mt-2 gap-2">
               <button
                 onClick={() => removeFromCart(item.id)}
-                className="bg-gray-500 text-white px-2 py-1 rounded hover:bg-gray-600 transition"
+                className="bg-red-700 h-7 text-white px-2 rounded-3xl hover:bg-red-800 transition"
               >
                 Eliminar
               </button>
+              <div className="flex justify-between items-center gap-2 border-2 border-gray-300 rounded-3xl max-w-[160px]">
+                <button
+                  onClick={() => removeOneFromCart(item.id)}
+                  className="px-2 font-black text-red-600"
+                >
+                  -
+                </button>
+                <p className="text-gray-600">Cantidad: {item.quantity}</p>
+
+                <button
+                  onClick={() => addToCart({ ...item, quantity: 1 })}
+                  className="px-2 font-black text-green-600"
+                >
+                  +
+                </button>
+              </div>
             </div>
           </div>
-          <p className="font-bold">${item.price * item.quantity}</p>
+          <div className="flex flex-col items-end">
+            <p className="text-gray-600">Precio: ${item.price}</p>
+            <p className="text-gray-600">Subtotal:</p>
+
+            <p className="font-bold">${item.price * item.quantity}</p>
+          </div>
         </div>
       ))}
       <div className="flex justify-between items-center bg-gray-200 p-4 rounded-lg shadow mt-4">
